330d2dd4b89eee13534ba1c5cb57711b39b34fc8
imibtc <113363320+imibtc@users.noreply.github.com>
2025-11-26 23:28:17 +0800

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -151,7 +151,7 @@ export default function Page() {
 
 					<div className='mt-8 flex items-center justify-center gap-6'>
 						<motion.a
-							href='https://github.com/YYsuni/2025-blog-public'
+							href='https://www.imibtc.com'
 							target='_blank'
 							rel='noreferrer'
 							initial={{ opacity: 0, scale: 0.6 }}
