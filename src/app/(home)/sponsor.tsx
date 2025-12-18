808aea9ecf55e929f464b8499e25de2c81f75d1a
yingshen-hk <54313133+yingshen-hk@users.noreply.github.com>
2025-12-18 21:01:54 +0800

    Create sponsor.tsx

---
+++ src/app/(home)/sponsor.tsx
@@ -0,0 +1,1 @@
+<iframe src="https://yingshen-hk.github.io/donate/" style="overflow-x:hidden;overflow-y:hidden; border:0xp none #fff; min-height:240px; width:100%;"  frameborder="0" scrolling="no"></iframe>
