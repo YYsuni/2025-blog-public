d99121d5b30e817bd049ddc593bbdcd706285b57
yingshen-hk <54313133+yingshen-hk@users.noreply.github.com>
2025-12-18 21:08:09 +0800

    Delete src/app/(home)/sponsor.tsx

--- src/app/(home)/sponsor.tsx
+++
@@ -1,1 +0,0 @@
-<iframe src="https://yingshen-hk.github.io/donate/" style="overflow-x:hidden;overflow-y:hidden; border:0xp none #fff; min-height:240px; width:100%;"  frameborder="0" scrolling="no"></iframe>
