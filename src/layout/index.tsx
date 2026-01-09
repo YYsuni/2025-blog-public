528896dc69943beafef3cd6cff4c77656ce41e54
appbeta2022 <appbeta@gmail.com>
2026-01-09 08:31:37 +0800

    cover
    aa

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -8,11 +8,12 @@ import { CircleCheckIcon, InfoIcon, Loader2Icon, OctagonXIcon, TriangleAlertIcon
 import { useSize, useSizeInit } from '@/hooks/use-size'
 import { useConfigStore } from '@/app/(home)/stores/config-store'
 import { ScrollTopButton } from '@/components/scroll-top-button'
+import MusicCard from '@/components/music-card'
 
 export default function Layout({ children }: PropsWithChildren) {
 	useCenterInit()
 	useSizeInit()
-	const { siteContent, regenerateKey } = useConfigStore()
+	const { cardStyles, siteContent, regenerateKey } = useConfigStore()
 	const { maxSM, init } = useSize()
 
 	const backgroundImages = (siteContent.backgroundImages ?? []) as Array<{ id: string; url: string }>
@@ -55,6 +56,8 @@ export default function Layout({ children }: PropsWithChildren) {
 			<main className='relative z-10 h-full'>
 				{children}
 				<NavCard />
+
+				{!maxSM && cardStyles.musicCard?.enabled !== false && <MusicCard />}
 			</main>
 
 			{maxSM && init && <ScrollTopButton className='bg-brand/20 fixed right-6 bottom-8 z-50 shadow-md' />}
