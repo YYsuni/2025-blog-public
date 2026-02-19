29e87533e6517eacec5be67ef63326171d3a23ae
WardGill <2063700935@qq.com>
2026-02-19 21:17:14 +0800

    Update GITHUB_APP_ID constant value

--- src/consts.ts
+++ src/consts.ts
@@ -11,6 +11,6 @@ export const GITHUB_CONFIG = {
 	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || '64m',
 	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || '2025-blog-public',
 	BRANCH: process.env.NEXT_PUBLIC_GITHUB_BRANCH || 'main',
-	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '2899522',
+	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '2899765',
 	ENCRYPT_KEY: process.env.NEXT_PUBLIC_GITHUB_ENCRYPT_KEY || 'wudishiduomejimo',
 } as const
