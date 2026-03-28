e560ebf8bb87966f45a529f380c1e486c2f43573
PYantian <1779074976@qq.com>
2026-03-28 20:05:11 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.css'
 
 import type { Metadata } from 'next'
-//import Layout from '@/layout'		//为了不在登录页面出现这个组件，所以注释掉
+import Layout from '@/layout'		//为了不在登录页面出现这个组件，所以注释掉
 import Layout from '@/layout'
 import Head from '@/layout/head'
 import siteContent from '@/config/site-content.json'
