d916d56979ddc89b9234fc081c89a2db5705618c
PYantian <1779074976@qq.com>
2026-03-30 12:26:31 +0800

    Refactor config store to use clone function for state
    Replaced object cloning method with a clone function for siteContent and cardStyles.

--- src/app/(main)/(home)/stores/config-store.ts
+++ src/app/(main)/(home)/stores/config-store.ts
@@ -5,41 +5,48 @@ import cardStyles from '@/config/card-styles.json'
 export type SiteContent = typeof siteContent
 export type CardStyles = typeof cardStyles
 
+const clone = <T>(value: T): T => JSON.parse(JSON.stringify(value))
+
 interface ConfigStore {
-	siteContent: SiteContent
-	cardStyles: CardStyles
-	regenerateKey: number
-	configDialogOpen: boolean
-	setSiteContent: (content: SiteContent) => void
-	setCardStyles: (styles: CardStyles) => void
-	resetSiteContent: () => void
-	resetCardStyles: () => void
-	regenerateBubbles: () => void
-	setConfigDialogOpen: (open: boolean) => void
+  siteContent: SiteContent
+  cardStyles: CardStyles
+  regenerateKey: number
+  configDialogOpen: boolean
+  setSiteContent: (content: SiteContent) => void
+  setCardStyles: (styles: CardStyles) => void
+  resetSiteContent: () => void
+  resetCardStyles: () => void
+  regenerateBubbles: () => void
+  setConfigDialogOpen: (open: boolean) => void
 }
 
-export const useConfigStore = create<ConfigStore>((set, get) => ({
-	siteContent: { ...siteContent },
-	cardStyles: { ...cardStyles },
-	regenerateKey: 0,
-	configDialogOpen: false,
-	setSiteContent: (content: SiteContent) => {
-		set({ siteContent: content })
-	},
-	setCardStyles: (styles: CardStyles) => {
-		set({ cardStyles: styles })
-	},
-	resetSiteContent: () => {
-		set({ siteContent: { ...siteContent } })
-	},
-	resetCardStyles: () => {
-		set({ cardStyles: { ...cardStyles } })
-	},
-	regenerateBubbles: () => {
-		set(state => ({ regenerateKey: state.regenerateKey + 1 }))
-	},
-	setConfigDialogOpen: (open: boolean) => {
-		set({ configDialogOpen: open })
-	}
-}))
+export const useConfigStore = create<ConfigStore>(set => ({
+  siteContent: clone(siteContent),
+  cardStyles: clone(cardStyles),
+  regenerateKey: 0,
+  configDialogOpen: false,
 
+  setSiteContent: (content: SiteContent) => {
+    set({ siteContent: content })
+  },
+
+  setCardStyles: (styles: CardStyles) => {
+    set({ cardStyles: styles })
+  },
+
+  resetSiteContent: () => {
+    set({ siteContent: clone(siteContent) })
+  },
+
+  resetCardStyles: () => {
+    set({ cardStyles: clone(cardStyles) })
+  },
+
+  regenerateBubbles: () => {
+    set(state => ({ regenerateKey: state.regenerateKey + 1 }))
+  },
+
+  setConfigDialogOpen: (open: boolean) => {
+    set({ configDialogOpen: open })
+  }
+}))
